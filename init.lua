d5522a5f03234388f92561c13f99ab933cd6d021
Mark Hamilton <crzytrane@gmail.com>
2024-03-05 22:27:16 +1300

    Got tailwind working by letting the defaults do the work

--- init.lua
+++ init.lua
@@ -733,6 +733,7 @@ local servers = {
   -- rust_analyzer = {},
   -- tsserver = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
+  --tailwindcss = { filetypes = { 'cshtml', 'css', 'html', 'jsx', 'tsx' } },
 
   lua_ls = {
     Lua = {
